4ea4610d317cb3c37780a7cae3dc5f9b5a18f69d
pranyajain3 <108532843+pranyajain3@users.noreply.github.com>
2023-08-02 21:25:54 +0530

    Add files via upload

---
+++ Move_File.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+
+from_dir = "C:/Users/Pranya Jain/Downloads"
+to_dir = "C:/Users/Pranya Jain/Downloads/Coding"
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
+for file_name in list_of_files:
+  name, extension = os.path.splitext(file_name)
+  print(name)
+  print(extension)
